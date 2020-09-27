7837159423e59dfb54cf152e5b2d41748f7350b2
Yuki Esaki <yesaki610@gmail.com>
2020-09-27 15:45:06 +0900

    todoアプリの完成

---
+++ todoApp/ListTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ListTableViewCell.swift
+//  todoApp
+//
+//  Created by esaki yuki on 2020/09/27.
+//  Copyright © 2020 esaki yuKki. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+    
+    @IBOutlet var tangoLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- todoApp/ViewController.swift
+++ todoApp/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var tangoTextField: UITextField!
+    
+    var wordArray: [Dictionary<String, String>] = []
+    
+    let saveData = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tangoTextField.delegate = self
+        
+        if saveData.array(forKey: "WORD") != nil {
+            wordArray = saveData.array(forKey: "WORD") as! [Dictionary<String, String>]
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+           textField.resignFirstResponder()
+           return true
+       }
+    
+    @IBAction func saveWord() {
+        let wordDictionary = ["tango": tangoTextField.text!]
+        wordArray.append(wordDictionary)
+        saveData.set(wordArray, forKey: "WORD")
+//        present(alert, animated: true, completion: nil)
+        tangoTextField.text = ""
+        self.navigationController?.popViewController(animated: true)
     }
 
 
